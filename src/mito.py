9d0ccb3bd435e7f22cf2854df8c3608b563730c0
Mauro Silberberg <maurosilber@gmail.com>
2024-08-02 17:32:30 -0300

    Rewrite ARM with mito module

---
+++ src/mito.py
@@ -0,0 +1,314 @@
+from simbio import Compartment, Parameter, Species, assign, initial, reactions
+
+
+class Mitochondria(Compartment):
+    KF: Parameter = assign(default=1e-6)
+    KR: Parameter = assign(default=1e-3)
+    KC: Parameter = assign(default=1)
+    volume: Parameter = assign(default=0.07)
+    pore_transport_rate: Parameter = assign(default=10)
+    transloc_rate: Parameter = assign(default=1e-2)
+
+    CytoC_C: Species = initial()
+    Smac_C: Species = initial()
+    Bax_A: Species = initial()
+
+    Bcl2: Species = initial(default=2e4)
+    CytoC_M: Species = initial(default=1e5)
+    Smac_M: Species = initial(default=1e5)
+    Mito_I: Species = initial(default=5e5)
+    Mito_A: Species = initial(default=0)
+    Bax: Species = initial(default=0)
+    Bax2: Species = initial(default=0)
+    Bax4: Species = initial(default=0)
+
+    r_Bax_transloc = reactions.Equilibration(
+        A=Bax_A,
+        B=Bax,
+        forward_rate=transloc_rate,
+        reverse_rate=transloc_rate,
+    )
+
+    r_Bax_dimerization = reactions.Equilibration(
+        A=2 * Bax,
+        B=Bax2,
+        forward_rate=KF / volume,
+        reverse_rate=KR,
+    )
+    r_Bax_tetramerization = reactions.Equilibration(
+        A=2 * Bax2,
+        B=Bax4,
+        forward_rate=KF / volume,
+        reverse_rate=KR,
+    )
+    r_Bax_Bcl2 = reactions.ReversibleSynthesis(
+        A=Bax,
+        B=Bcl2,
+        AB=0,
+        forward_rate=KF / volume,
+        reverse_rate=KR,
+    )
+    r_Bax2_Bcl2 = reactions.ReversibleSynthesis(
+        A=Bax2,
+        B=Bcl2,
+        AB=0,
+        forward_rate=KF / volume,
+        reverse_rate=KR,
+    )
+    r_Bax4_Bcl2 = reactions.ReversibleSynthesis(
+        A=Bax4,
+        B=Bcl2,
+        AB=0,
+        forward_rate=KF / volume,
+        reverse_rate=KR,
+    )
+    r_Bax4_Mito = reactions.CatalyzeConvert(
+        A=Bax4,
+        B=Mito_I,
+        AB=0,
+        P=Mito_A,
+        forward_rate=KF / volume,
+        reverse_rate=KR,
+        conversion_rate=KC,
+    )
+    r_Smac_pore = reactions.MichaelisMenten(
+        E=Mito_A,
+        S=Smac_M,
+        ES=0,
+        P=Smac_C,
+        forward_rate=2 * KF / volume,
+        reverse_rate=KR,
+        catalytic_rate=pore_transport_rate,
+    )
+    r_CytoC_pore = reactions.MichaelisMenten(
+        E=Mito_A,
+        S=CytoC_M,
+        ES=0,
+        P=CytoC_C,
+        forward_rate=2 * KF / volume,
+        reverse_rate=KR,
+        catalytic_rate=pore_transport_rate,
+    )
+
+
+class ARM(Compartment):
+    KF: Parameter = assign(default=1e-6)
+    KR: Parameter = assign(default=1e-3)
+    KC: Parameter = assign(default=1)
+    transloc_rate: Parameter = assign(default=1e-2)
+
+    L: Species = initial(default=0)
+    R: Species = initial(default=200)
+    DISC: Species = initial(default=0)
+    flip: Species = initial(default=100)
+    C8_pro: Species = initial(default=20_000)
+    C8_A: Species = initial(default=0)
+    BAR: Species = initial(default=1_000)
+    Bid_U: Species = initial(default=4e4)
+    Bid_T: Species = initial(default=0)
+    Bid_M: Species = initial(default=0)
+    Bax_C: Species = initial(default=1e5)
+    Bax_A: Species = initial(default=0)
+    CytoC_C: Species = initial(default=0)
+    CytoC_A: Species = initial(default=0)
+    Smac_C: Species = initial(default=0)
+    Smac_A: Species = initial(default=0)
+    Apaf_I: Species = initial(default=1e3)
+    Apaf_A: Species = initial(default=0)
+    Apop: Species = initial(default=0)
+    C3_pro: Species = initial(default=1e4)
+    C3_A: Species = initial(default=0)
+    C3_ub: Species = initial(default=0)
+    C6_pro: Species = initial(default=1e4)
+    C6_A: Species = initial(default=0)
+    PARP_U: Species = initial(default=1e6)
+    PARP_C: Species = initial(default=0)
+    XIAP: Species = initial(default=1e4)
+    Bcl2c: Species = initial(default=2e4)
+    IntrinsicStimuli: Species = initial(default=0)
+
+    mitocondria = Mitochondria(
+        KF=KF,
+        KR=KR,
+        KC=KC,
+        transloc_rate=transloc_rate,
+        CytoC_C=CytoC_C,
+        Smac_C=Smac_C,
+        Bax_A=Bax_A,
+    )
+
+    r_Smac_transloc = reactions.Equilibration(
+        A=Smac_C,
+        B=Smac_A,
+        forward_rate=transloc_rate,
+        reverse_rate=transloc_rate,
+    )
+    r_CytoC_transloc = reactions.Equilibration(
+        A=CytoC_C,
+        B=CytoC_A,
+        forward_rate=transloc_rate,
+        reverse_rate=transloc_rate,
+    )
+
+    # Reactions
+    r_L_R = reactions.CatalyzeConvert(
+        A=L,
+        B=R,
+        AB=0,
+        P=DISC,
+        forward_rate=4e-7,
+        reverse_rate=KR,
+        conversion_rate=1e-5,
+    )
+    r_DISC_C8 = reactions.MichaelisMenten(
+        E=DISC,
+        S=C8_pro,
+        ES=0,
+        P=C8_A,
+        forward_rate=KF,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
+    r_C8_Bid = reactions.MichaelisMenten(
+        E=C8_A,
+        S=Bid_U,
+        ES=0,
+        P=Bid_T,
+        forward_rate=1e-7,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
+    r_DISC_flip = reactions.ReversibleSynthesis(
+        A=DISC,
+        B=flip,
+        AB=0,
+        forward_rate=KF,
+        reverse_rate=KR,
+    )
+    r_BAR_C8 = reactions.ReversibleSynthesis(
+        A=BAR,
+        B=C8_A,
+        AB=0,
+        forward_rate=KF,
+        reverse_rate=KR,
+    )
+    r_CytoC_Apaf = reactions.MichaelisMenten(
+        E=CytoC_A,
+        S=Apaf_I,
+        ES=0,
+        P=Apaf_A,
+        forward_rate=5e-7,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
+    r_ApafA_C3pro = reactions.MichaelisMenten(
+        E=Apaf_A,
+        S=C3_pro,
+        ES=0,
+        P=C3_A,
+        forward_rate=5e-09,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
+    r_C3A_ApafA = reactions.MichaelisMenten(
+        E=C3_A,
+        S=Apaf_A,
+        ES=0,
+        P=Apop,
+        forward_rate=1.3e-06,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
+    r_Apop_C3 = reactions.MichaelisMenten(
+        E=Apop,
+        S=C3_pro,
+        ES=0,
+        P=C3_A,
+        forward_rate=5e-9,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
+    r_ApafA_XIAP = reactions.ReversibleSynthesis(
+        A=Apaf_A,
+        B=XIAP,
+        AB=0,
+        forward_rate=2e-6,
+        reverse_rate=KR,
+    )
+    r_Smac_XIAP = reactions.ReversibleSynthesis(
+        A=Smac_A,
+        B=XIAP,
+        AB=0,
+        forward_rate=7e-6,
+        reverse_rate=KR,
+    )
+    r_C8_C3 = reactions.MichaelisMenten(
+        E=C8_A,
+        S=C3_pro,
+        ES=0,
+        P=C3_A,
+        forward_rate=1e-7,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
+    r_XIAP_C3 = reactions.MichaelisMenten(
+        E=XIAP,
+        S=C3_A,
+        ES=0,
+        P=C3_ub,
+        forward_rate=2e-6,
+        reverse_rate=KR,
+        catalytic_rate=1e-1,
+    )
+    r_C3_PARP = reactions.MichaelisMenten(
+        E=C3_A,
+        S=PARP_U,
+        ES=0,
+        P=PARP_C,
+        forward_rate=KF,
+        reverse_rate=1e-2,
+        catalytic_rate=KC,
+    )
+    r_C3_C6 = reactions.MichaelisMenten(
+        E=C3_A,
+        S=C6_pro,
+        ES=0,
+        P=C6_A,
+        forward_rate=KF,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
+    r_C6_C8 = reactions.MichaelisMenten(
+        E=C6_A,
+        S=C8_pro,
+        ES=0,
+        P=C8_A,
+        forward_rate=3e-8,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
+    r_Bid_Bax = reactions.MichaelisMenten(
+        E=Bid_T,
+        S=Bax_C,
+        ES=0,
+        P=Bax_A,
+        forward_rate=1e-7,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
+    r_Bcl2_Bid = reactions.ReversibleSynthesis(
+        A=Bid_T,
+        B=Bcl2c,
+        AB=0,
+        forward_rate=KF,
+        reverse_rate=KR,
+    )
+    r_intrinsic = reactions.MichaelisMenten(
+        E=IntrinsicStimuli,
+        S=Bid_U,
+        ES=0,
+        P=Bid_T,
+        forward_rate=KF,
+        reverse_rate=KR,
+        catalytic_rate=KC,
+    )
---
+++ src/tests/test_mito.py
@@ -0,0 +1,97 @@
+import numpy as np
+import pandas as pd
+from poincare import solvers
+from simbio import Simulator
+
+from ..corbat import IntrinsicARM as ARM
+from ..mito import ARM as ARM_Mito
+
+
+def test_mito():
+    mapping = {
+        ARM.IntrinsicStimuli: ARM_Mito.IntrinsicStimuli,
+        ARM.arm.L: ARM_Mito.L,
+        ARM.arm.L: ARM_Mito.L,
+        ARM.arm.R: ARM_Mito.R,
+        ARM.arm.DISC: ARM_Mito.DISC,
+        ARM.arm.flip: ARM_Mito.flip,
+        ARM.arm.C8_pro: ARM_Mito.C8_pro,
+        ARM.arm.C8_A: ARM_Mito.C8_A,
+        ARM.arm.BAR: ARM_Mito.BAR,
+        ARM.arm.Bid_U: ARM_Mito.Bid_U,
+        ARM.arm.Bid_T: ARM_Mito.Bid_T,
+        ARM.arm.Bid_M: ARM_Mito.Bid_M,
+        ARM.arm.Bax_C: ARM_Mito.Bax_C,
+        ARM.arm.Bax_M: ARM_Mito.mitocondria.Bax,
+        ARM.arm.Bax_A: ARM_Mito.Bax_A,
+        ARM.arm.Bcl2: ARM_Mito.mitocondria.Bcl2,
+        ARM.arm.CytoC_M: ARM_Mito.mitocondria.CytoC_M,
+        ARM.arm.CytoC_C: ARM_Mito.CytoC_C,
+        ARM.arm.CytoC_A: ARM_Mito.CytoC_A,
+        ARM.arm.Smac_M: ARM_Mito.mitocondria.Smac_M,
+        ARM.arm.Smac_C: ARM_Mito.Smac_C,
+        ARM.arm.Smac_A: ARM_Mito.Smac_A,
+        ARM.arm.Apaf_I: ARM_Mito.Apaf_I,
+        ARM.arm.Apaf_A: ARM_Mito.Apaf_A,
+        ARM.arm.Apop: ARM_Mito.Apop,
+        ARM.arm.C3_pro: ARM_Mito.C3_pro,
+        ARM.arm.C3_A: ARM_Mito.C3_A,
+        ARM.arm.C3_ub: ARM_Mito.C3_ub,
+        ARM.arm.C6_pro: ARM_Mito.C6_pro,
+        ARM.arm.C6_A: ARM_Mito.C6_A,
+        ARM.arm.PARP_U: ARM_Mito.PARP_U,
+        ARM.arm.PARP_C: ARM_Mito.PARP_C,
+        ARM.arm.XIAP: ARM_Mito.XIAP,
+        ARM.arm.Bax_M2: ARM_Mito.mitocondria.Bax2,
+        ARM.arm.Bax_M4: ARM_Mito.mitocondria.Bax4,
+        ARM.arm.Mito_I: ARM_Mito.mitocondria.Mito_I,
+        ARM.arm.Mito_A: ARM_Mito.mitocondria.Mito_A,
+        ARM.arm.Bcl2c: ARM_Mito.Bcl2c,
+        ARM.arm.r_Bax2_Bcl2.AB: ARM_Mito.mitocondria.r_Bax2_Bcl2.AB,
+        ARM.arm.r_Bax4_Bcl2.AB: ARM_Mito.mitocondria.r_Bax4_Bcl2.AB,
+        ARM.arm.r_Bax4_Mito.AB: ARM_Mito.mitocondria.r_Bax4_Mito.AB,
+        ARM.arm.r_Bax_Bcl2.AB: ARM_Mito.mitocondria.r_Bax_Bcl2.AB,
+        ARM.arm.r_CytoC_pore.ES: ARM_Mito.mitocondria.r_CytoC_pore.ES,
+        ARM.arm.r_Smac_pore.ES: ARM_Mito.mitocondria.r_Smac_pore.ES,
+        ARM.arm.r_ApafA_C3pro.ES: ARM_Mito.r_ApafA_C3pro.ES,
+        ARM.arm.r_ApafA_XIAP.AB: ARM_Mito.r_ApafA_XIAP.AB,
+        ARM.arm.r_Apop_C3.ES: ARM_Mito.r_Apop_C3.ES,
+        ARM.arm.r_BAR_C8.AB: ARM_Mito.r_BAR_C8.AB,
+        ARM.arm.r_Bcl2_Bid.AB: ARM_Mito.r_Bcl2_Bid.AB,
+        ARM.arm.r_Bid_Bax.ES: ARM_Mito.r_Bid_Bax.ES,
+        ARM.arm.r_C3A_ApafA.ES: ARM_Mito.r_C3A_ApafA.ES,
+        ARM.arm.r_C3_C6.ES: ARM_Mito.r_C3_C6.ES,
+        ARM.arm.r_C3_PARP.ES: ARM_Mito.r_C3_PARP.ES,
+        ARM.arm.r_C6_C8.ES: ARM_Mito.r_C6_C8.ES,
+        ARM.arm.r_C8_Bid.ES: ARM_Mito.r_C8_Bid.ES,
+        ARM.arm.r_C8_C3.ES: ARM_Mito.r_C8_C3.ES,
+        ARM.arm.r_CytoC_Apaf.ES: ARM_Mito.r_CytoC_Apaf.ES,
+        ARM.arm.r_DISC_C8.ES: ARM_Mito.r_DISC_C8.ES,
+        ARM.arm.r_DISC_flip.AB: ARM_Mito.r_DISC_flip.AB,
+        ARM.arm.r_L_R.AB: ARM_Mito.r_L_R.AB,
+        ARM.arm.r_Smac_XIAP.AB: ARM_Mito.r_Smac_XIAP.AB,
+        ARM.arm.r_XIAP_C3.ES: ARM_Mito.r_XIAP_C3.ES,
+        ARM.r_intrinsic.ES: ARM_Mito.r_intrinsic.ES,
+    }
+
+    t = np.linspace(0, 30_000, 1_000)
+    solver = solvers.LSODA(atol=1e-6, rtol=1e-6)
+    df_ARM = Simulator(ARM).solve(
+        solver=solver,
+        save_at=t,
+        values={ARM.arm.L: 1000, ARM.IntrinsicStimuli: 0},
+    )
+    df_Mito = Simulator(ARM_Mito).solve(
+        solver=solver,
+        save_at=t,
+        values={ARM_Mito.L: 1000, ARM_Mito.IntrinsicStimuli: 0},
+    )
+
+    df_ARM.rename(inplace=True, columns={str(k): str(v) for k, v in mapping.items()})
+    pd.testing.assert_frame_equal(
+        df_ARM,
+        df_Mito,
+        atol=1e-4,
+        rtol=1e-4,
+        check_like=True,  # ignore order
+    )
